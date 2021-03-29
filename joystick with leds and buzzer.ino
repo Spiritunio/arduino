0b586f64e0e01351abaadff8f3b1fe2613505041
Spiritunio <64743345+Spiritunio@users.noreply.github.com>
2021-03-29 08:39:33 +0200

    Update joystick with leds and buzzer.ino

--- joystick with leds and buzzer.ino
+++ joystick with leds and buzzer.ino
@@ -6,14 +6,15 @@ int green = 11; // connect green led to pin 11
 int yellow = 9; // connect yellow led to pin 9
 int blue = 8; // connect blue led to pin 8
 int buzzer = 5; // connect buzzer to pin 5
-void setup() 
-{ 
+
+void setup() { 
   pinMode(buzzer, OUTPUT); // pin modes
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(blue, OUTPUT);
   pinMode(zpin, INPUT); 
+
   Serial.begin(9600); // start serial monitor
 }
 void loop() 
@@ -22,39 +23,40 @@ void loop()
   x=analogRead(xpin); // read x position
   y=analogRead(ypin); // read y position
   z=digitalRead(zpin); // read z position
+  
   Serial.print(x ,DEC); //send positions to serial monitor
   Serial.print(",");
   Serial.print(y ,DEC);
   Serial.print(",");
   Serial.println(z ,DEC);
+  
   if (x == 0){
-  digitalWrite(green, HIGH); // if joystick go to right, run green led
+    digitalWrite(green, HIGH); // if joystick go to right, run green led
+  } else {
+    digitalWrite (green, LOW);
   }
-  else {
-  digitalWrite (green, LOW);
-  }
+  
   if (x == 1023) {
     digitalWrite(blue, HIGH); // if left, then run blue led
-  }
-  else {
+  } else {
     digitalWrite(blue, LOW);
   }
-    if (y == 1023) {
+  
+  if (y == 1023) {
     digitalWrite(red, HIGH); // if up, then run red led
-  }
-  else {
+  } else {
     digitalWrite(red, LOW);
   }
-    if (y == 0) {
+  
+  if (y == 0) {
     digitalWrite(yellow, HIGH); // if down, then run yellow led
-  }
-  else {
+  } else {
     digitalWrite(yellow, LOW);
   }
+  
   if (z == 1) {
     tone(buzzer, 700); // if pressed, run buzzer
-  }
-  else {
+  } else {
     noTone(buzzer);
   }
 
