85abf2938dd2cb4dd6917889841064a941c00b03
Spiritunio <64743345+Spiritunio@users.noreply.github.com>
2021-03-29 08:31:30 +0200

    Update joystick with leds and buzzer.ino

--- joystick with leds and buzzer.ino
+++ joystick with leds and buzzer.ino
@@ -1,58 +1,58 @@
-int xpin = 0;
-int ypin = 1;
-int zpin = 3;
-int red = 13;
-int green = 11;
-int yellow = 9;
-int blue = 8;
-int buzzer = 5;
+int xpin = 0; // connect x to A0
+int ypin = 1; // connect y to A1
+int zpin = 3; // connect z to pin 3
+int red = 13; // connect red led to pin 13
+int green = 11; // connect green led to pin 11
+int yellow = 9; // connect yellow led to pin 9
+int blue = 8; // connect blue led to pin 8
+int buzzer = 5; // connect buzzer to pin 5
 void setup() 
 { 
-  pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT); // pin modes
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
   pinMode(blue, OUTPUT);
   pinMode(zpin, INPUT); 
-  Serial.begin(9600); // 9600 bps
+  Serial.begin(9600); // start serial monitor
 }
 void loop() 
 { 
   int x,y,z;
-  x=analogRead(xpin);
-  y=analogRead(ypin);
-  z=digitalRead(zpin);
-  Serial.print(x ,DEC);
+  x=analogRead(xpin); // read x position
+  y=analogRead(ypin); // read y position
+  z=digitalRead(zpin); // read z position
+  Serial.print(x ,DEC); //send positions to serial monitor
   Serial.print(",");
   Serial.print(y ,DEC);
   Serial.print(",");
   Serial.println(z ,DEC);
   if (x == 0){
-  digitalWrite(green, HIGH);
+  digitalWrite(green, HIGH); // if joystick go to right, run green led
   }
   else {
   digitalWrite (green, LOW);
   }
   if (x == 1023) {
-    digitalWrite(blue, HIGH);
+    digitalWrite(blue, HIGH); // if left, then run blue led
   }
   else {
     digitalWrite(blue, LOW);
   }
     if (y == 1023) {
-    digitalWrite(red, HIGH);
+    digitalWrite(red, HIGH); // if up, then run red led
   }
   else {
     digitalWrite(red, LOW);
   }
     if (y == 0) {
-    digitalWrite(yellow, HIGH);
+    digitalWrite(yellow, HIGH); // if down, then run yellow led
   }
   else {
     digitalWrite(yellow, LOW);
   }
   if (z == 1) {
-    tone(buzzer, 700);
+    tone(buzzer, 700); // if pressed, run buzzer
   }
   else {
     noTone(buzzer);
